d340afa566a859930a9b2863e716ad24158f34df
Paweł <48798582+pafim37@users.noreply.github.com>
2021-01-24 21:48:34 +0100

    Create an architecture of the program

---
+++ Quadratic-function/Quadratic-function/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Quadratic_function
+{
+    class Class1
+    {
+    }
+}
---
+++ Quadratic-function/Quadratic-function/QuadraticFunction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Quadratic_function
+{
+    class QuadraticFunction
+    {
+        QuadraticFunction() { }
+        QuadraticFunction(double a) { }
+        QuadraticFunction(double a, double b) { }
+        QuadraticFunction(double a, double b, double c) { }
+        
+        public double A { get; private set; }
+        public double B { get; private set; }
+        public double C { get; private set; }
+
+        public void ShowFunction() { }
+
+        public void ShowZeros() { }
+    }
+}
