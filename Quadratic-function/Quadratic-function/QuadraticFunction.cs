b7e4acc2ef74c9f3c676c8dafdcc4f32f26b5630
Paweł <48798582+pafim37@users.noreply.github.com>
2021-01-24 21:53:55 +0100

    User can create quadratic function

--- Quadratic-function/Quadratic-function/QuadraticFunction.cs
+++ Quadratic-function/Quadratic-function/QuadraticFunction.cs
@@ -6,10 +6,32 @@ namespace Quadratic_function
 {
     class QuadraticFunction
     {
-        QuadraticFunction() { }
-        QuadraticFunction(double a) { }
-        QuadraticFunction(double a, double b) { }
-        QuadraticFunction(double a, double b, double c) { }
+        public QuadraticFunction()
+        {
+            A = 0;
+            B = 0;
+            C = 0;
+        }
+
+        public QuadraticFunction(double a) 
+        {
+            A = a;
+            B = 0;
+            C = 0;
+        }
+        public QuadraticFunction(double a, double b)
+        { 
+            A = a;
+            B = b;
+            C = 0;
+        }
+
+        public QuadraticFunction(double a, double b, double c) 
+        {
+            A = a;
+            B = b;
+            C = c;
+        }
         
         public double A { get; private set; }
         public double B { get; private set; }
